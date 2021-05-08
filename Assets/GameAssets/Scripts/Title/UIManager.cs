8f826ffe1d99bed886e1438c1e04b0233796299c
Zekiah-A <zekiahamoako@outlook.com>
2021-05-08 13:46:54 +0100

    Full UI usability update, scene management and cleanup. Lobby and lighting update

---
+++ Assets/GameAssets/Scripts/Title/UIManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void LoadMultiplayer()
+    {
+        try
+        {
+            SceneManager.LoadScene("Multiplayer");
+        } catch(Exception _e)
+        {
+            Debug.Log($"Could not load scene Multiplayer: {_e}");
+        }
+    }
+
+    public void TopBarEnter(GameObject _underline)
+    {
+        _underline.transform.localScale = new Vector3(1.5f ,_underline.transform.localScale.y, _underline.transform.localScale.z);
+    }
+
+    public void TopBarExit(GameObject _underline)
+    {
+        _underline.transform.localScale = new Vector3(1, _underline.transform.localScale.y, _underline.transform.localScale.z);
+    }
+}
